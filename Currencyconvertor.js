65d9c1622c9ec2f2d69b01072a776b2349cf232e
Haider ALi <haideralithakur747@gmail.com>
2025-08-22 15:37:05 +0500

    80%

--- Currencyconvertor.js
+++ Currencyconvertor.js
@@ -1,16 +1,45 @@
+
+let url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json";
+const getcurrencydata = async () => {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+};
+const fromCurrency = document.querySelector("#from-currency");
+const toCurrency = document.querySelector("#to-currency");
+const amount = document.querySelector("#amount");
 const dropdowns = document.querySelectorAll(".dropdown select");
+const result = document.querySelector("#result");
+let final_value ;
+let toRate;
+let fromRate;
+function convertCurrency(){
+  getcurrencydata().then(data => {
+     fromRate = data.eur[fromCurrency.value.toLowerCase()];
+    
+     toRate = data.eur[toCurrency.value.toLowerCase()];
+    
+    let convertedAmount = (amount / fromRate) * toRate;
+    result.innerText = `${amount} ${fromCurrency.value} = ${convertedAmount.toFixed(2)} ${toCurrency.value}`;
+final_value= convertedAmount;  
+  });
+
+}
+convertCurrency();
+
 for (let select of dropdowns){
   for(let currcode in countryList){
   let newOption = document.createElement("option");
   newOption.innerText = currcode;
-  newOption.textContent = currcode;
-  if(select.name==="from"&& currcode==="USD")
+  newOption.value = currcode;
+  if(select.name==="from-currency"&& currcode==="USD")
   {
     newOption.selected = true;
-  }if(select.name==="to"&& currcode==="PKR")
+  }if(select.name==="to-currency"&& currcode==="PKR")
   {
     newOption.selected = true;
   }
+  // updateflag(select);
   select.appendChild(newOption);
 
   select.addEventListener("change", (e) => {
