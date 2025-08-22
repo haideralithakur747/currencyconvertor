b8ba03a172f6919f6135cdab1982972922def798
Haider ALi <haideralithakur747@gmail.com>
2025-08-22 17:02:22 +0500

    complete

--- Currencyconvertor.js
+++ Currencyconvertor.js
@@ -1,5 +1,5 @@
-
-let url = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json";
+let url =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json";
 const getcurrencydata = async () => {
   let response = await fetch(url);
   let data = await response.json();
@@ -10,49 +10,67 @@ const toCurrency = document.querySelector("#to-currency");
 const amount = document.querySelector("#amount");
 const dropdowns = document.querySelectorAll(".dropdown select");
 const result = document.querySelector("#result");
-let final_value ;
+const buttonn = document.querySelector("#convert-button");
+
+let final_value;
 let toRate;
 let fromRate;
-function convertCurrency(){
-  getcurrencydata().then(data => {
-     fromRate = data.eur[fromCurrency.value.toLowerCase()];
+
+function convertCurrency() {
+  getcurrencydata().then((data) => {
+    fromRate = data.eur[fromCurrency.value.toLowerCase()];
+
+  console.log(fromRate);
+    toRate = data.eur[toCurrency.value.toLowerCase()];
+
+    let convertedAmount = (amount.value / fromRate) * toRate;
+    console.log(amount);
+    result.innerText = `${amount.value} ${
+      fromCurrency.value
+    } = ${convertedAmount.toFixed(2)} ${toCurrency.value}`;
+    final_value = convertedAmount;4
     
-     toRate = data.eur[toCurrency.value.toLowerCase()];
-    
-    let convertedAmount = (amount / fromRate) * toRate;
-    result.innerText = `${amount} ${fromCurrency.value} = ${convertedAmount.toFixed(2)} ${toCurrency.value}`;
-final_value= convertedAmount;  
+updateResult();
   });
+}
+
+  convertCurrency();
+function updateResult() {
+  let result1 = fromRate / toRate;
 
 }
-convertCurrency();
 
-for (let select of dropdowns){
-  for(let currcode in countryList){
-  let newOption = document.createElement("option");
-  newOption.innerText = currcode;
-  newOption.value = currcode;
-  if(select.name==="from-currency"&& currcode==="USD")
-  {
-    newOption.selected = true;
-  }if(select.name==="to-currency"&& currcode==="PKR")
-  {
-    newOption.selected = true;
+
+
+for (let select of dropdowns) {
+  for (let currcode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currcode;
+    newOption.value = currcode;
+    if (select.name === "from-currency" && currcode === "USD") {
+      newOption.selected = true;
+    }
+    if (select.name === "to-currency" && currcode === "PKR") {
+      newOption.selected = true;
+    }
+    // updateflag(select);
+    select.appendChild(newOption);
+
+    select.addEventListener("change", (e) => {
+      updateflag(e.target);
+    });
   }
-  // updateflag(select);
-  select.appendChild(newOption);
-
-  select.addEventListener("change", (e) => {
-    updateflag(e.target);
-  });
 }
-}
-const updateflag = (element) =>{
-
+const updateflag = (element) => {
   let currcode = element.value;
-  console.log(currcode)
-    let countryCode = countryList[currcode];
+  console.log(currcode);
+  let countryCode = countryList[currcode];
   let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
- let img = element.parentElement.querySelector("img");
- img.src = newSrc;
-}
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+buttonn.addEventListener("click", (e)=> {
+  e.preventDefault();
+  convertCurrency();
+});
